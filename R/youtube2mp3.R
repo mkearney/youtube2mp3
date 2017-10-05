6fb3df9ade1129f1018cefc729487e70a3204e99
Michael W. Kearney <kearneymw@missouri.edu>
2017-10-04 20:04:27 -0500

    now using browseURL + wait for conversion download method

--- R/youtube2mp3.R
+++ R/youtube2mp3.R
@@ -2,7 +2,9 @@
 #'
 #' Converts youtube links to mp3s.
 #' 
-#' @param youtube_url Link to youtube video. URL must include "youtube.com/watch?v=".
+#' @param video_id Youtube's assigned unique identifier; also accepted is a
+#'   link to a youtube video URL, though if a URL is provided, it must
+#'   include "youtube.com/watch?v=".
 #' @param file Name of output mp3 file. A ".mp3" file extension will be added if not
 #'   already included. This defaults to the video code (value after v= in the URL) and
 #'   saved in the current working directory.
@@ -17,24 +19,42 @@
 #' )
 #' }
 #' @importFrom httr GET content stop_for_status
-youtube2mp3 <- function(youtube_url, file = NULL) {
-  stopifnot(grepl("www\\.youtube\\.com\\/watch\\?v\\=", youtube_url))
-  youtube_code <- gsub(".*=", "", youtube_url)
+youtube2mp3 <- function(video_id, file = NULL) {
+  stopifnot(is.character(video_id), length(video_id) == 1L)
+  if (grepl("^http|^www", video_id)) {
+    stopifnot(grepl("www\\.youtube\\.com\\/watch\\?v\\=", video_id))
+    video_id <- gsub(".*=", "", video_id)
+  }
   mp3_url <- paste0(
     "http://www.yt-mp3.com/watch?v=",
-    youtube_code
+    video_id
   )
-  txt <- httr::GET(mp3_url)
-  httr::stop_for_status(txt)
-  txt <- httr::content(txt, "text", encoding = "UTF-8")
+  tfse::menuline
+  browseURL(mp3_url)
+  sh <- menuline(
+    "Has the video \"successfully been converted\" yet?",
+    c("Yes", "No"))
+  if (sh == 2L) {
+    stop("Well then I don't know what to tell you", call. = FALSE)
+  }
+  r <- httr::GET(mp3_url)
+  httr::stop_for_status(r)
+  txt <- httr::content(r, "text", encoding = "UTF-8")
   m <- gregexpr("(?<=video_url\":\")[^\"]*(?=\"\\})", txt, perl = TRUE)
-  vidurl <- regmatches(txt, m)
-  vidurl <- paste0("http:", gsub("\\/video", "", vidurl))
+  m <- regmatches(txt, m)[[1]]
   if (is.null(file)) {
-    file <- paste0(youtube_code, ".mp3")
+    file <- paste0(video_id, ".mp3")
   } else if (!grepl("\\.mp3$", file)) {
     file <- paste0(file, ".mp3")
   }
   stopifnot(is.character(file))
+  vidurl <- paste0("http:", gsub("\\/video", "", m))
+  stopifnot(length(vidurl) == 1, is.character(vidurl))
+  message("Download link is ", vidurl)
   download.file(vidurl, file)
+}
+
+menuline <- function(q, a) {
+  message(q)
+  menu(a)
 }
--- make.R
+++ make.R
@@ -1,3 +1,34 @@
 ## R make file
 devtools::document()
 devtools::install()
+
+# install.packages("devtools")
+
+if (!"tube" %in% installed.packages()) {
+  devtools::install_github(
+    "soodoku/tuber",
+    build_vignettes = TRUE
+  )  
+}
+
+## oautherize yahoo api sesssion
+tuber::yt_oauth(
+  Sys.getenv("YOUTUBE_APP_ID"),
+  Sys.getenv("YOUTUBE_APP_SECRET"),
+)
+
+## search for leftovers theme
+x <- tuber::yt_search('leftovers')
+pos <- grep("leftover.*theme", x$title, ignore.case = TRUE)
+
+## view search hits with leftovers and theme
+x$title[pos]
+
+## select video to convert to mp3
+vid <- as.character(x$video_id[1])
+
+## pass vid to youtube2mp3 fun
+youtube2mp3(vid)
+
+## once video has converted enter "1" to proceed with download
+1
